7e1f26a51dcc20a7056659a7f84d44726ec60297
Kaomi <maksim.burmistrov2006@gmail.com>
2026-04-16 17:43:55 +0300

    Коризна

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
-function App() {
-return (
-    <div style={{marginLeft: "15px"}}>
+function Catalog() {
+  return (
+    <div className="katalog">
       <h1>Каталог книг</h1>
 
-      <p>
-        Корзина <span id="kolvo"></span>
-      </p>
+      <Link to="/korzina">
+        Корзина <span id="kolvo">0</span>
+      </Link>
 
-      <div style={{ display: "flex", gap: "15px" }}>
-        <div className="card">
-          <p>Книга 1</p>
-          <p>Достоевский</p>
-          <p>100 ₽</p>
-          <button>В корзину</button>
-        </div>
+      <div id="catalog">
+        <div style={{ display: "flex", gap: "15px" }}>
+          <div className="card">
+            <p>Книга 1</p>
+            <p>Автор 1</p>
+            <p>500₽</p>
+            <button>В корзину</button>
+          </div>
 
-        <div className="card">
-          <p>Книга 2</p>
-          <p>Пушкин</p>
-          <p>200 ₽</p>
-          <button>В корзину</button>
-        </div>
+          <div className="card">
+            <p>Книга 2</p>
+            <p>Автор 2</p>
+            <p>700₽</p>
+            <button>В корзину</button>
+          </div>
 
-        <div className="card">
-          <p>Книга 3</p>
-          <p>Гоголь</p>
-          <p>300 ₽</p>
-          <button>В корзину</button>
+          <div className="card">
+            <p>Книга 3</p>
+            <p>Автор 3</p>
+            <p>1000₽</p>
+            <button>В корзину</button>
+          </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function Korzina() {
+  return (
+    <div className="katalog">
+      <h1>Корзина</h1>
+
+      <Link to="/">Каталог</Link>
+
+      <p>Количество: 0</p>
+
+      <div id="korzList"></div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Catalog />} />
+      <Route path="/korzina" element={<Korzina />} />
+    </Routes>
   );
 }
 
